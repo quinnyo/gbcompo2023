19aa59707e8eacb367b0bfbd097994c32dd9480d
quinnyo <3379314+quinnyo@users.noreply.github.com>
2023-09-22 00:08:13 +1000

    terrain tileset: remove leftover collision shapes from pier thing

--- src/res/map/terrain.tsx
+++ src/res/map/terrain.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="terrain" class="bg" tilewidth="8" tileheight="8" tilecount="128" columns="16">
+<tileset version="1.10" tiledversion="1.10.2" name="terrain" class="bg" tilewidth="8" tileheight="8" tilecount="128" columns="16">
  <properties>
   <property name="gigantgolf_tileset" value="bg"/>
  </properties>
@@ -10,66 +10,5 @@
   <properties>
    <property name="gigantgolf_tileset" value="bg"/>
   </properties>
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="49">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="50">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="51">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="52">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="53">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="54">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="55">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
- </tile>
- <tile id="56">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0">
-    <polyline points="0,0 8,0"/>
-   </object>
-  </objectgroup>
  </tile>
 </tileset>

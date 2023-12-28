31918e57f43e46036798f24f9184738f2cfbeac5
quinnyo <3379314+quinnyo@users.noreply.github.com>
2023-12-28 15:47:35 +1000

    Add collision boxes to `buildings` tiles. Add `CollisionShape` class to Tiled project.

--- src/res/map/buildings.tsx
+++ src/res/map/buildings.tsx
@@ -1,65 +1,172 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="buildings" class="fg" tilewidth="8" tileheight="8" tilecount="48" columns="16">
+<tileset version="1.10" tiledversion="1.10.2" name="buildings" class="fg" tilewidth="8" tileheight="8" tilecount="48" columns="16">
  <properties>
   <property name="gigantgolf_tileset" value="fg"/>
  </properties>
  <image source="buildings.png" width="128" height="24"/>
  <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="2" y="1" width="5" height="7"/>
+  </objectgroup>
   <animation>
    <frame tileid="0" duration="500"/>
    <frame tileid="1" duration="500"/>
    <frame tileid="2" duration="500"/>
   </animation>
  </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="3" width="7" height="5"/>
+  </objectgroup>
+ </tile>
  <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="0" width="7" height="8"/>
+  </objectgroup>
   <animation>
    <frame tileid="3" duration="500"/>
    <frame tileid="4" duration="500"/>
    <frame tileid="5" duration="500"/>
   </animation>
  </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="3" width="8" height="5"/>
+  </objectgroup>
+ </tile>
  <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="1" width="8" height="7"/>
+  </objectgroup>
   <animation>
    <frame tileid="6" duration="500"/>
    <frame tileid="7" duration="500"/>
    <frame tileid="8" duration="500"/>
   </animation>
  </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="3" width="8" height="5"/>
+  </objectgroup>
+ </tile>
  <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="1" width="8" height="7"/>
+  </objectgroup>
   <animation>
    <frame tileid="9" duration="500"/>
    <frame tileid="10" duration="500"/>
    <frame tileid="11" duration="500"/>
   </animation>
  </tile>
+ <tile id="10">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="2" width="8" height="6"/>
+  </objectgroup>
+ </tile>
  <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="1" width="7" height="7"/>
+  </objectgroup>
   <animation>
    <frame tileid="12" duration="500"/>
    <frame tileid="13" duration="500"/>
    <frame tileid="14" duration="500"/>
   </animation>
  </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="3" width="8" height="5"/>
+  </objectgroup>
+ </tile>
  <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="2" y="1" width="3" height="7"/>
+  </objectgroup>
   <animation>
    <frame tileid="16" duration="500"/>
    <frame tileid="57" duration="1000"/>
   </animation>
  </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="1" width="7" height="7"/>
+  </objectgroup>
+ </tile>
  <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="4" width="4" height="4"/>
+  </objectgroup>
   <animation>
    <frame tileid="18" duration="500"/>
    <frame tileid="57" duration="500"/>
    <frame tileid="57" duration="500"/>
   </animation>
  </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="1" width="6" height="7"/>
+  </objectgroup>
+ </tile>
  <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="2" y="1" width="5" height="7"/>
+  </objectgroup>
   <animation>
    <frame tileid="20" duration="500"/>
    <frame tileid="57" duration="500"/>
    <frame tileid="57" duration="500"/>
   </animation>
  </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="1" width="7" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="2" y="2" width="6" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="23">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="3" y="1" width="4" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="2" width="8" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="3" y="3" width="5" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="2" width="6" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="28">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="1" width="6" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="30">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="1" y="1" width="6" height="7"/>
+  </objectgroup>
+ </tile>
  <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="0" width="7" height="8"/>
+  </objectgroup>
   <animation>
    <frame tileid="32" duration="500"/>
    <frame tileid="17" duration="500"/>
@@ -67,6 +174,9 @@
   </animation>
  </tile>
  <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="0" width="6" height="8"/>
+  </objectgroup>
   <animation>
    <frame tileid="34" duration="500"/>
    <frame tileid="19" duration="500"/>
@@ -74,10 +184,38 @@
   </animation>
  </tile>
  <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="0" width="6" height="8"/>
+  </objectgroup>
   <animation>
    <frame tileid="36" duration="500"/>
    <frame tileid="21" duration="500"/>
    <frame tileid="37" duration="500"/>
   </animation>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="0" width="7" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="2" width="6" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" type="CollisionShape" x="1" y="2" width="6" height="6"/>
+  </objectgroup>
  </tile>
 </tileset>
